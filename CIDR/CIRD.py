c61ed93a9ae9e0229cdca57b07a7317dad7663a1
PratikGarai <pratikgarai0208@gmail.com>
2020-09-18 05:16:39 +0530

    More code

--- CIDR/CIRD.py
+++ CIDR/CIRD.py
@@ -1,5 +1,15 @@
 import sys
 
+def processed(d):
+    return [d[:8],d[8:16],d[16:24],d[24:]]
+    
+def getFirstLast(c,n):
+    s = ''.join(c)
+    f = s[:n]+'0'*(32-n)
+    l = s[:n]+'1'*(32-n)
+
+    return [processed(f),processed(l)]
+
 def toBinary(c):
     s = []
     for i in c:
@@ -15,9 +25,11 @@ def main():
     c = toBinary(ci.split('.'))
     n = int(n)
 
+    first, last = getFirstLast(c,n)
+
     print(c)
-    # print(f)
-    # print(l)
+    print(first)
+    print(last)
 
 if __name__=='__main__':
     main()
