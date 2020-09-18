9a9f6b9ea57bac794cb9fd11353c9751dd817000
PratikGarai <pratikgarai0208@gmail.com>
2020-09-18 05:32:11 +0530

    completed basic cidr

--- CIDR/CIRD.py
+++ CIDR/CIRD.py
@@ -1,5 +1,8 @@
 import sys
 
+def numericals(s):
+    return [ int(i,2) for i in s ]
+
 def processed(d):
     return [d[:8],d[8:16],d[16:24],d[24:]]
     
@@ -21,15 +24,32 @@ def toBinary(c):
 def main():
     ci = sys.argv[1]
     ci, n = ci.split('/')
-
-    c = toBinary(ci.split('.'))
     n = int(n)
 
-    first, last = getFirstLast(c,n)
+    if len(sys.argv)>2:
+        if sys.argv[2]=='n':
+            c = toBinary(ci.split('.'))
+        else :
+            c = ci.split('.')
+    else:
+            c = toBinary(ci.split('.'))
 
-    print(c)
-    print(first)
-    print(last)
+    first, last = getFirstLast(c,n)
+    
+    print('\n')
+    print('Binary')
+    print('Original : ',c)
+    print('First    : ',first)
+    print('Last     : ',last)
+
+    print('\n')
+    print('Numericals')
+    print('Original : ',numericals(c))
+    print('First    : ',numericals(first))
+    print('Last     : ',numericals(last))
+
+    print()
+    print('Number   : ',2**(32-n))
 
 if __name__=='__main__':
     main()
