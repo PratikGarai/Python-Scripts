4cdf9c91649ef3d6d37ccf7a6185f47263470eda
PratikGarai <pratikgarai0208@gmail.com>
2021-12-26 22:25:37 +0530

    Install.sh added

---
+++ PScripts/install-all.sh
@@ -0,0 +1,4 @@
+pip install flair
+pip install requests==2.23.0 folium==0.2.1
+pip install scispacy==0.2.5
+pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_core_sci_sm-0.2.5.tar.gz
